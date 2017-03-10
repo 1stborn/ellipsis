c4c3df78453f7795fcf4582a1f613fca8a3a0833
Firstborn <1stborm@gmail.com>
2017-03-10 23:52:03 +0200

    auth module rework

--- src/auth.php
+++ src/auth.php
@@ -2,32 +2,79 @@
 namespace Ellipsis;
 
 use Corpus\Config;
+use Corpus\Session;
+use GuzzleHttp\Client;
 
-class Auth extends Di {
-	public function authorize($userData) {
-		if ( is_scalar($userData) )
-			$userData = json_decode($userData, true);
+/**
+ * Class Auth
+ *
+ * @property Client  http
+ * @property DB      db
+ * @property Session session
+ *
+ * @package Ellipsis
+ */
+abstract class Auth extends Di {
+	abstract protected function save($data);
 
-		return $this->session->user = $userData;
-	}
+	abstract public function create($id, $provider);
 
-	public function isValid() {
+	abstract public function account($id, $provider);
+
+	public function valid() {
 		return (bool)$this->session->user;
 	}
 
-	public function getName() {
-		return $this->getUserData('name');
+	public function get($name) {
+		return $this->valid() ? $this->session->user[$name] : null;
 	}
 
-	public function getUserData($field = null, $default = null) {
-		return $field ? $this->session->user[$field] ?? $default : $this->session->user;
-	}
-
-	public function setUserData($userData) {
-		return $this->session->user = Config::merge((array)$this->session->user, $userData);
+	public function set($name, $value = null) {
+		if ( is_array($name) ) {
+			$this->session->user = Config::merge($this->session->user ?? [], $name);
+			$this->save($name);
+		}
+		else {
+			$this->session->user = Config::merge($this->session->user ?? [], [$name =>$value]);
+			$this->save([$name => $value]);
+		}
 	}
 
 	public function logout() {
 		$this->session->user = null;
+	}
+
+	public function upload($image) {
+		if ( $this->valid() ) {
+			$id = $this->session->user['id'];
+
+			if ( $info = getimagesizefromstring($image) ) {
+				switch ($info[2]) {
+					case IMAGETYPE_JPEG:
+					case IMAGETYPE_JPEG2000:
+						$name = $id . '.jpg';
+					break;
+					case IMAGETYPE_GIF:
+						$name = $id . '.gif';
+					break;
+					case IMAGETYPE_PNG:
+						$name = $id . '.png';
+					break;
+					default:
+						$name = $id;
+				}
+
+				return file_put_contents(
+					Config::get('settings.public.images') . $name, $image
+				) ? $name : false;
+			}
+		}
+
+		return false;
+	}
+
+	public function authorize($data) {
+
+		$this->session->user = $data;
 	}
 }
